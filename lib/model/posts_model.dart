d48266f819d142423da8e6b94b537aa7312c7135
Tzeyi <tytiong2@gmail.com>
2024-03-11 16:23:35 -0400

    moved model into services

--- lib/model/posts_model.dart
+++
@@ -1,8 +0,0 @@
-class Post {
-  final String imagePath;
-  final String caption;
-  final String likes;
-  final String comments;
-
-  Post({required this.imagePath, required this.caption, required this.likes, required this.comments});
-}
